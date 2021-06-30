1d8c720e4330cc0be6b85483712b677f1c1ddfe2
leandropicoli <leandropicoli@live.com>
2021-06-29 21:29:04 -0300

    Deleted some trash.

--- src/NerdStore.Sells.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NerdStore.Sells.Application
-{
-    public class Class1
-    {
-    }
-}
--- src/NerdStore.Sells.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NerdStore.Sells.Domain
-{
-    public class Class1
-    {
-    }
-}
